4711678ff0b10d322f3871ae5e3511b167ec5f56
llewmihs <joeshimwell@gmail.com>
2018-01-02 15:34:32 +0000

    f,b,l,r

--- joebotv1.py
+++ joebotv1.py
@@ -2,28 +2,28 @@ import explorerhat
 from time import sleep
 
 def move(direction):
-    if direction == "forward":
+    if direction == "f":
         print("Moving forward...")
         explorerhat.motor.one.forward()
         explorerhat.motor.two.forward()
         sleep(0.5)
         explorerhat.motor.one.stop()
         explorerhat.motor.two.stop()
-    if direction == "backward":
+    if direction == "b":
         explorerhat.motor.one.backward()
         explorerhat.motor.two.backward()
         sleep(0.5)
         explorerhat.motor.one.stop()
         explorerhat.motor.two.stop()
         print("Moving backward...")
-    if direction == "left":
+    if direction == "l":
         print("turning left...")
         explorerhat.motor.one.forward()
         explorerhat.motor.two.backward()
         sleep(0.5)
         explorerhat.motor.one.stop()
         explorerhat.motor.two.stop()
-    if direction == "right":
+    if direction == "r":
         print("Turning right...")
         explorerhat.motor.one.backward()
         explorerhat.motor.two.forward()
